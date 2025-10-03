935283491f977eb50bd2cb0d97524b7fac6c1dc1
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2025-10-03 13:43:41 +1000

    ASYNC function test;

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,17 @@
 export const addFunc = (a: number, b: number): number => {
   return a + b;
 };
+
+interface PostInterface {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+
+export const getPostsFromDB = async (): Promise<PostInterface[]> => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) throw new Error('Network response was not ok');
+  return res.json();
+};
